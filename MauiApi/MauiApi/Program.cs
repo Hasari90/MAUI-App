2ce6e72b263782bc339ef2410e8a65df4fcba172
Hubert Janus <janus.hubert@wp.pl>
2022-09-29 12:19:45 +0200

    Adding the rest of the methods

--- MauiApi/MauiApi/Program.cs
+++ MauiApi/MauiApi/Program.cs
@@ -1,4 +1,5 @@
 using MauiApi.Data;
+using MauiApi.Models;
 using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -24,6 +25,53 @@ app.MapGet("api/users", async (AppDbContext context) =>
     var users = await context.Users.ToListAsync();
 
     return Results.Ok(users);
+});
+
+app.MapPost("api/users", async (AppDbContext context, User user) =>
+{ 
+    await context.Users.AddAsync(user); 
+    await context.SaveChangesAsync();
+
+    return Results.Created($"api/users/{user.Id}", user);
+});
+
+app.MapPut("api/users/{id}", async (AppDbContext context, int id, User user) =>
+{
+    var userModel = await context.Users.FirstOrDefaultAsync(x => x.Id == id);   
+
+    if(userModel == null)
+    {
+        return Results.NotFound();
+    }
+    else
+    {
+        userModel.Name = user.Name; 
+        userModel.Surname = user.Surname;
+
+        await context.SaveChangesAsync();
+
+        return Results.Ok(userModel);
+    }
+
+    return Results.NoContent();
+});
+
+app.MapDelete("api/users/{id}", async (AppDbContext context, int id) =>
+{
+    var userModel = await context.Users.FirstOrDefaultAsync(x => x.Id == id);
+
+    if (userModel == null)
+    {
+        return Results.NotFound();
+    }
+    else
+    {
+        context.Users.Remove(userModel);    
+
+        await context.SaveChangesAsync();
+
+        return Results.NoContent();
+    }
 });
 
 app.Run();
